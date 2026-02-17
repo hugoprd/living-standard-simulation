ca11f698a35a01d579d18c074fb1498857262537
hugoprd <hugo.prd.br@gmail.com>
2026-02-17 12:10:41 -0300

    Atualiza logger, permitindo a criação de outros logs dependendo do processo rodado

--- logs/set_logger.py
+++ logs/set_logger.py
@@ -1,12 +1,34 @@
 # logs/set_logger.py
 from loguru import logger
 from pathlib import Path
+from datetime import datetime
 
 
-def setup_logger(log_file_path: Path):
+def setup_logger(log_file_path: Path, log_name: str, overwrite: bool):
+    """
+    Configura o log.
+
+    Se overwrite=True, limpa o arquivo antigo.
+    Se overwrite=False, cria um arquivo novo com data e hora.
+    """
+
+    if overwrite:
+        log_file = log_file_path / f"{log_name}.txt"
+        mode = "w"  # faz com que o arquivo .txt de log seja regerado sempre que o código for rodado
+    else:
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        log_file = log_file_path / f"{log_name}_{timestamp}.txt"
+        mode = "a"  # faz com que dê 'append' (escreve depois do que já estava la)
+
+    rotation = "10 MB"  # pra evitar que o txt fique infinito
+
+    if "model_training" in log_name:
+        rotation = "150 MB"  # muito fácil dos arquivos de log de treinamento do modelo ultrapassarem os 100MB
+
     logger.add(
-        log_file_path,
+        log_file,
         format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
         level="DEBUG",
-        rotation="10 MB",  # evita que o txt fique infinito
+        rotation=rotation,  # evita que o txt fique infinito
+        mode=mode,
     )
